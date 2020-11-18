756daea85a673018a27490d84c8eaf989b75fb06
isiahsimas <isiahsimas@users.noreply.github.com>
2020-11-17 23:38:13 -0500

    Completed structure to create survey

--- test1/index.js
+++ test1/index.js
@@ -16,10 +16,30 @@ var json = {
                         "1750-1800", "1800-1850", "1850-1900", "1900-1950", "after 1950"
                     ],
                     correctAnswer: "1850-1900"
+                },
+                {
+                    type: "radiogroup",
+                    name: "civilwar",
+                    title: "When was the Civil War?",
+                    choices: [
+                        "1750-1800", "1800-1850", "1850-1900", "1900-1950", "after 1950"
+                    ],
+                    correctAnswer: "1850-1900"
                 }
             ]
-        }, {
+        }, 
+        {
             questions: [
+                {
+                    type: "radiogroup",
+                    name: "libertyordeath",
+                    title: "Who said 'Give me liberty or give me death?'",
+                    choicesOrder: "random",
+                    choices: [
+                        "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
+                    ],
+                    correctAnswer: "Patrick Henry"
+                },
                 {
                     type: "radiogroup",
                     name: "libertyordeath",
@@ -31,8 +51,67 @@ var json = {
                     correctAnswer: "Patrick Henry"
                 }
             ]
-        }, {
+        }, 
+        {
             questions: [
+                {
+                    type: "radiogroup",
+                    name: "magnacarta",
+                    title: "What is the Magna Carta?",
+                    choicesOrder: "random",
+                    choices: [
+                        "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
+                    ],
+                    correctAnswer: "The foundation of the British parliamentary system"
+                },
+                {
+                    type: "radiogroup",
+                    name: "magnacarta",
+                    title: "What is the Magna Carta?",
+                    choicesOrder: "random",
+                    choices: [
+                        "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
+                    ],
+                    correctAnswer: "The foundation of the British parliamentary system"
+                }
+            ]
+        },
+        {
+            questions: [
+                {
+                    type: "radiogroup",
+                    name: "magnacarta",
+                    title: "What is the Magna Carta?",
+                    choicesOrder: "random",
+                    choices: [
+                        "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
+                    ],
+                    correctAnswer: "The foundation of the British parliamentary system"
+                },
+                {
+                    type: "radiogroup",
+                    name: "magnacarta",
+                    title: "What is the Magna Carta?",
+                    choicesOrder: "random",
+                    choices: [
+                        "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
+                    ],
+                    correctAnswer: "The foundation of the British parliamentary system"
+                }
+            ]
+        },
+        {
+            questions: [
+                {
+                    type: "radiogroup",
+                    name: "magnacarta",
+                    title: "What is the Magna Carta?",
+                    choicesOrder: "random",
+                    choices: [
+                        "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
+                    ],
+                    correctAnswer: "The foundation of the British parliamentary system"
+                },
                 {
                     type: "radiogroup",
                     name: "magnacarta",
--- test2/single.js
+++ test2/single.js
@@ -35,6 +35,74 @@ var json = {
                     "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
                 ],
                 correctAnswer: "The foundation of the British parliamentary system"
+            },
+            {
+                type: "radiogroup",
+                name: "civilwar",
+                title: "When was the Civil War?",
+                choices: [
+                    "1750-1800", "1800-1850", "1850-1900", "1900-1950", "after 1950"
+                ],
+                correctAnswer: "1850-1900"
+            },
+            {
+                type: "radiogroup",
+                name: "libertyordeath",
+                title: "Who said 'Give me liberty or give me death?'",
+                choicesOrder: "random",
+                choices: [
+                    "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
+                ],
+                correctAnswer: "Patrick Henry"
+            },
+            {
+                type: "radiogroup",
+                name: "magnacarta",
+                title: "What is the Magna Carta?",
+                choicesOrder: "random",
+                choices: [
+                    "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
+                ],
+                correctAnswer: "The foundation of the British parliamentary system"
+            },
+            {
+                type: "radiogroup",
+                name: "civilwar",
+                title: "When was the Civil War?",
+                choices: [
+                    "1750-1800", "1800-1850", "1850-1900", "1900-1950", "after 1950"
+                ],
+                correctAnswer: "1850-1900"
+            },
+            {
+                type: "radiogroup",
+                name: "libertyordeath",
+                title: "Who said 'Give me liberty or give me death?'",
+                choicesOrder: "random",
+                choices: [
+                    "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
+                ],
+                correctAnswer: "Patrick Henry"
+            },
+            {
+                type: "radiogroup",
+                name: "magnacarta",
+                title: "What is the Magna Carta?",
+                choicesOrder: "random",
+                choices: [
+                    "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
+                ],
+                correctAnswer: "The foundation of the British parliamentary system"
+            },
+            {
+                type: "radiogroup",
+                name: "magnacarta",
+                title: "What is the Magna Carta?",
+                choicesOrder: "random",
+                choices: [
+                    "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
+                ],
+                correctAnswer: "The foundation of the British parliamentary system"
             }
         ]
     }],
